950e4879e60faec62abb2c1bcd54fcf412f594bb
Emmanuel Akinyemi <58298846+emmanuel-akinyemi-Dev@users.noreply.github.com>
2025-11-11 04:47:48 +0100

    add feat:personal details, contact details

---
+++ app/(tabs)/settings/help/payment.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function payment() {
+  return (
+    <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
+      <Text style={{color:"black"}}>payment</Text>
+    </View>
+  )""
+}
+
+const styles = StyleSheet.create({})
